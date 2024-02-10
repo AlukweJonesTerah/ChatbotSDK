42568e6837958704354b7014018dc394e70aae91
blackcode-creator <legacyitsolution@gmail.com>
2024-02-10 21:20:10 +0300

    added: script

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ from wtforms.validators import DataRequired, Email, ValidationError
 import bcrypt
 from flask_mysqldb import MySQL
 import pymysql
-from scripts.database_setup import setup_database 
+from scripts.database_setup import setup_database
+from scripts.auto_create import create_project_structure
 
 
 setup_database()
+create_project_structure('./apikey.json', '1234')
+
+
 
 app = Flask(__name__)
 
@@ -48,6 +52,10 @@ class LoginForm(FlaskForm):
 @app.route('/')
 def index():
     return render_template('dashboard.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
 
 @app.route('/register',methods=['GET','POST'])
 def register():
@@ -108,7 +116,7 @@ def dashboard():
 def logout():
     session.pop('user_id', None)
     flash("You have been logged out successfully.")
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 
 

55a20d79641592c139be7bd079fcc905e0457b93
blackcode-creator <legacyitsolution@gmail.com>
2024-02-15 21:37:01 +0300

    kindly checkout from this branch

--- app.py
+++ app.py
@@ -1,76 +1,77 @@
-from flask import Flask, render_template, redirect, url_for, session, flash
+from flask import Flask, render_template, redirect, url_for, session, flash, request, jsonify
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
 import bcrypt
-from flask_mysqldb import MySQL
 import pymysql
-from scripts.database_setup import setup_database
-from scripts.auto_create import create_project_structure
-from flask import Flask, render_template, request
-import random
-import json
-from keras.models import load_model
-import webbrowser
-import numpy as np
-import pickle
-from nltk.stem import WordNetLemmatizer
-import nltk
-nltk.download('popular')
-
-#setup_database()
-create_project_structure('./apikey.json', '1234')
-
-
+import requests
+import base64
+from datetime import datetime
+import json  
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+def connect_to_database():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        database='db',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
-app.secret_key = 'your_secret_key_here'
-
-connection=pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='chatbotsdk_user'
-)
-
-
+def get_access_token():
+    consumer_key = "ZXFRgG801YwWVTnS1YOA4p9LlVXdudNgJV8Pnf6SJ8zlOVOn"  
+    consumer_secret = "SGILYZJtPJkNtIHuhtaiVxkEVaLno4YgN7yhak2PaGB1AFzIBBmgZAqXIKowb2AR"  
+    access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    headers = {'Content-Type': 'application/json'}
+    auth = (consumer_key, consumer_secret)
+    try:
+        response = requests.get(access_token_url, headers=headers, auth=auth)
+        response.raise_for_status() 
+        result = response.json()
+        access_token = result['access_token']
+        return {'access_token': access_token}
+    except requests.exceptions.RequestException as e:
+        return {'error': str(e)}
 
 class RegisterForm(FlaskForm):
-    name = StringField("Name",validators=[DataRequired()])
-    email = StringField("Email",validators=[DataRequired(), Email()])
-    password = PasswordField("Password",validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Register")
 
-    def validate_email(self,field):
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM users where email=%s",(field.data,))
-        user = cur.fetchone()
-        cur.close()
-        if user:
-            raise ValidationError('Email Already Taken')
+    def validate_email(self, field):
+        with connect_to_database().cursor() as cur:
+            cur.execute("SELECT * FROM users where email=%s", (field.data,))
+            user = cur.fetchone()
+            if user:
+                raise ValidationError('Email Already Taken')
 
 class LoginForm(FlaskForm):
-    email = StringField("Email",validators=[DataRequired(), Email()])
-    password = PasswordField("Password",validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
-
-
 
 @app.route('/')
 def index():
-    return render_template('dashboard.html')
-
-@app.route('/about')
-def about_page():
-    return render_template('about.html')
+    get_access_token()
+    return render_template('home.html')
 
 @app.route('/pricing')
 def pricing():
     return render_template('pricing.html')
 
-@app.route('/register',methods=['GET','POST'])
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -78,51 +79,49 @@ def register():
         email = form.email.data
         password = form.password.data
 
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-        # store data into database 
-        cur = connection.cursor()
-        cur.execute("INSERT INTO users (name,email,password) VALUES (%s,%s,%s)",(name,email,hashed_password))
-        connection.commit()
-        cur.close()
+        # Store data into database
+        with connect_to_database().cursor() as cur:
+            cur.execute("INSERT INTO users (name,email,password) VALUES (%s,%s,%s)", (name, email, hashed_password))
+            connect_to_database().commit()
 
         return redirect(url_for('login'))
 
-    return render_template('register.html',form=form)
+    return render_template('register.html', form=form)
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
 
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM users WHERE email=%s",(email,))
-        user = cur.fetchone()
-        cur.close()
-        if user and bcrypt.checkpw(password.encode('utf-8'), user[3].encode('utf-8')):
-            session['user_id'] = user[0]
+        with connect_to_database().cursor() as cur:
+            cur.execute("SELECT * FROM users WHERE email=%s", (email,))
+            user = cur.fetchone()
+
+        if user and bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
+            session['user_id'] = user['id']
             return redirect(url_for('dashboard'))
         else:
             flash("Login failed. Please check your email and password")
             return redirect(url_for('login'))
 
-    return render_template('login.html',form=form)
+    return render_template('login.html', form=form)
 
 @app.route('/dashboard')
 def dashboard():
     if 'user_id' in session:
         user_id = session['user_id']
 
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM users where id=%s",(user_id,))
-        user = cur.fetchone()
-        cur.close()
+        with connect_to_database().cursor() as cur:
+            cur.execute("SELECT * FROM users where id=%s", (user_id,))
+            user = cur.fetchone()
 
         if user:
-            return render_template('dashboard.html',user=user)
-            
+            return render_template('dashboard.html', user=user)
+
     return redirect(url_for('login'))
 
 @app.route('/logout')
@@ -132,81 +131,6 @@ def logout():
     return redirect(url_for('index'))
 
 
-
-# trial chatbot
-
-
-lemmatizer = WordNetLemmatizer()
-model = load_model('./chatbots/Councellor/model.counsellor')
-intents = json.loads(open('./chatbots/Councellor/data.json', "r+", encoding="utf-8").read())
-words = pickle.load(open('./chatbots/Councellor/texts.pkl', 'rb'))
-classes = pickle.load(open('./chatbots/Councellor/labels.pkl', 'rb'))
-
-
-def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(
-        word.lower()) for word in sentence_words]
-    return sentence_words
-
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
-
-def bow(sentence, words, show_details=True):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)
-    for s in sentence_words:
-        for i, w in enumerate(words):
-            if w == s:
-                # assign 1 if current word is in the vocabulary position
-                bag[i] = 1
-                if show_details:
-                    print("found in bag: %s" % w)
-    return(np.array(bag))
-
-
-def predict_class(sentence, model):
-    # filter out predictions below a threshold
-    p = bow(sentence, words, show_details=False)
-    res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
-
-
-def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag'] == tag):
-            result = random.choice(i['responses'])
-            break
-    return result
-
-
-def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    res = getResponse(ints, intents)
-    return res
-
-
-
-
-
-
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return chatbot_response(userText)
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
